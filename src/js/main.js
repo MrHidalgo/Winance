10e57f22b6249b24a64e6462226165aaa03c078a
MrHidalgo <Luckyhidalgo@gmail.com>
2018-06-24 15:46:28 +0300

    Style input range and checkbox.

--- src/js/main.js
+++ src/js/main.js
@@ -321,6 +321,21 @@ $(document).ready(function(){
 
   //
   // ====================
+  $('input[type=range]').on('input', function(e){
+    var min = e.target.min,
+      max = e.target.max,
+      val = e.target.value;
+
+    $(e.target).css({
+      'backgroundSize': (val - min) * 100 / (max - min) + '% 100%'
+    });
+  }).trigger('input');
+  // ====================
+
+
+
+  //
+  // ====================
   function funcName() {}
   _document.on("click", "", function(e) {});
   // ====================
