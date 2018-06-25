72deed3e7f9f2f42f328e01e9c44c2d28379a814
MrHidalgo <Luckyhidalgo@gmail.com>
2018-06-25 14:48:16 +0300

    Add gulp babel for ES6.

--- gulp/tasks/javascript.js
+++ gulp/tasks/javascript.js
@@ -4,6 +4,7 @@ var util        = require('gulp-util');
 var plumber     = require('gulp-plumber');
 var concat      = require('gulp-concat');
 var uglifyJs    = require('gulp-uglify');
+var babel       = require('gulp-babel');
 var config      = require('../config');
 
 gulp.task('javascript:vendor', function() {
@@ -35,6 +36,9 @@ gulp.task('javascript:app', function() {
      ])
     .pipe(plumber({ errorHandler: config.errorHandler }))
     .pipe(concat('app.js'))
+    .pipe(babel({
+      "presets": ["env"]
+    }))
     .pipe(config.production ? uglifyJs() : util.noop())
     .pipe(gulp.dest(config.dest.js));
 });
