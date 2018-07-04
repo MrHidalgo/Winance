5e8a1253c92cc47b64be2ea3b3c82986954d8128
MrHidalgo <Luckyhidalgo@gmail.com>
2018-07-04 17:33:28 +0300

    Add faq popup.

--- src/js/main.js
+++ src/js/main.js
@@ -1106,7 +1106,7 @@ $(document).ready(function () {
       }
     });
 
-    $('[review-btn-js], [ticket-new-js]').magnificPopup({
+    $('[review-btn-js], [ticket-new-js], [faq-new-js]').magnificPopup({
       type: 'inline',
       fixedContentPos: true,
       fixedBgPos: true,
